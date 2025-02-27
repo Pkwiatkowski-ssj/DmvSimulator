935950254206dc45d946ad4178c2549d737fb3cc
pkwiatkowski-ssj <pkwiatkowski@quinnipiac.edu>
2025-02-26 19:31:40 -0500

    increased the range from 100 to 200

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -3,19 +3,19 @@ import java.util.Random;
 public class DmvSimulator {
     public static void main(String[] args) {
         Random random = new Random();
-        int userNumber = random.nextInt(100) + 1; // Random number between 1 and 100
+        int userNumber = random.nextInt(200) + 1;
         System.out.println("Your number is: " + userNumber);
         System.out.println("Please wait until your number is called...");
-        // Start calling numbers, beginning from the next number after user's
-        int currentNumber = (userNumber % 100) + 1;
+        
+        int currentNumber = (userNumber % 200) + 1;
 
         while (currentNumber != userNumber) {
             System.out.println("Calling number: " + currentNumber);
-            currentNumber = (currentNumber % 100) + 1; // Wrap around after 100
+            currentNumber = (currentNumber % 200) + 1; // 
         }
 
         // Finally, call the user's number last
         System.out.println("Calling number: " + userNumber);
-        System.out.println("Sorry, you don’t have the required paperwork.");
+        System.out.println("Sorry, you dont have the required paperwork.");
     }
 }
