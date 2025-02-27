225b0357ff7eaa9c3fadd2dd506b09930bb274ff
pkwiatkowski-ssj <pkwiatkowski@quinnipiac.edu>
2025-02-26 19:24:49 -0500

    calls out each number one at a time starting from the number directly after the one they have. prompts a message telling the user they dont have the required paperwork

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -6,6 +6,16 @@ public class DmvSimulator {
         int userNumber = random.nextInt(100) + 1; // Random number between 1 and 100
         System.out.println("Your number is: " + userNumber);
         System.out.println("Please wait until your number is called...");
+        // Start calling numbers, beginning from the next number after user's
+        int currentNumber = (userNumber % 100) + 1;
 
+        while (currentNumber != userNumber) {
+            System.out.println("Calling number: " + currentNumber);
+            currentNumber = (currentNumber % 100) + 1; // Wrap around after 100
+        }
+
+        // Finally, call the user's number last
+        System.out.println("Calling number: " + userNumber);
+        System.out.println("Sorry, you don’t have the required paperwork.");
     }
 }
